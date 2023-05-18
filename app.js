b4ea37d406ee9c9e14932c59af7c8c61c35d0895
Divya Kumari <102896170+Iamdivyak@users.noreply.github.com>
2023-05-18 13:57:54 +0530

    Update app.js
    added .Strategy in facebook variable

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const FacebookStrategy = require("passport-facebook");
+const FacebookStrategy = require("passport-facebook").Strategy;
 const findOrCreate = require("mongoose-findorcreate");
 const app = express();
 
